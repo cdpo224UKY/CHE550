78c5c3290018053fa64e5d3566c891414346adc6
cdpo224UKY <31912114+cdpo224UKY@users.noreply.github.com>
2017-10-09 01:54:29 -0400

    blah
    blah

--- Activities/activity_8.py
+++ Activities/activity_8.py
@@ -9,9 +9,33 @@ sys.path.append(CHE550_path)
 import CHE550_tools
 
 if __name__ == '__main__':
+
+    # human p53 tumor antigen
     p53_fasta_str, rq_time = CHE550_tools.ut.entry_retrieve_from_uniprot('fasta', 'P04637')
-    print(p53_fasta_str)
     p53_pro = CHE550_tools.pt.Protein.init_fasta_parse(p53_fasta_str)
+
     print(p53_pro.id)
     print(p53_pro.accession)
-    print(str(p53_pro.sequence))
+    print(CHE550_tools.pt.amino_acid_to_r_type(p53_pro.sequence))
+    CHE550_tools.pt.count_che(CHE550_tools.pt.amino_acid_to_r_type(p53_pro.sequence))
+    print()
+
+    # human DNA polymerase theta
+    p53_fasta_str, rq_time = CHE550_tools.ut.entry_retrieve_from_uniprot('fasta', 'O75417')
+    p53_pro = CHE550_tools.pt.Protein.init_fasta_parse(p53_fasta_str)
+
+    print(p53_pro.id)
+    print(p53_pro.accession)
+    print(CHE550_tools.pt.amino_acid_to_r_type(p53_pro.sequence))
+    CHE550_tools.pt.count_che(CHE550_tools.pt.amino_acid_to_r_type(p53_pro.sequence))
+    print()
+
+    # human DNA polymerase theta
+    p53_fasta_str, rq_time = CHE550_tools.ut.entry_retrieve_from_uniprot('fasta', 'Q7Z5Q5')
+    p53_pro = CHE550_tools.pt.Protein.init_fasta_parse(p53_fasta_str)
+
+    print(p53_pro.id)
+    print(p53_pro.accession)
+    print(CHE550_tools.pt.amino_acid_to_r_type(p53_pro.sequence))
+    CHE550_tools.pt.count_che(CHE550_tools.pt.amino_acid_to_r_type(p53_pro.sequence))
+    print()
--- CHE550_tools/protein_tools.py
+++ CHE550_tools/protein_tools.py
@@ -1,6 +1,45 @@
 #!/usr/bin/python3
 import CHE550_tools
 
+amino_acid_r_group_chemistry = {
+    'G': 'n', 'A': 'n', 'P': 'n', 'V': 'n',
+    'L': 'n', 'I': 'n', 'M': 'n',
+
+    'F': 'a', 'Y': 'a', 'W': 'a',
+
+    'S': 'u', 'C': 'u', 'T': 'u', 'N': 'u',
+    'Q': 'u',
+
+    'K': '+', 'H': '+', 'R': '+',
+
+    'D': '-', 'E': '-'
+}
+
+
+def amino_acid_to_r_type(pro_str):
+
+    r_group_che_str = str()
+
+    for x in range(0, len(pro_str)):
+        r_group_che_str += amino_acid_r_group_chemistry[pro_str[x]]
+
+    return r_group_che_str
+
+
+def count_che(pro_str):
+
+    n_count = pro_str.count('n')
+    a_count = pro_str.count('a')
+    u_count = pro_str.count('u')
+    p_count = pro_str.count('+')
+    e_count = pro_str.count('-')
+
+    print('n: ' + str(n_count) + " %: " + str(n_count/len(pro_str)))
+    print('a: ' + str(a_count) + " %: " + str(a_count/len(pro_str)))
+    print('u: ' + str(u_count) + " %: " + str(u_count/len(pro_str)))
+    print('+: ' + str(p_count) + " %: " + str(p_count/len(pro_str)))
+    print('-: ' + str(e_count) + " %: " + str(e_count/len(pro_str)))
+
 
 class Protein(object):
 
@@ -44,7 +83,7 @@ class Protein(object):
 
         accession = fasta_str[ac_s: ac_e]
         id = fasta_str[ac_e + 1: fasta_str.find(' ') + 1]
-        seq = fasta_str[fasta_str.find('\n'):]
+        seq = fasta_str[fasta_str.find('\n'):].replace('\n', '')
 
         return cls(id, accession, seq, None)
 
