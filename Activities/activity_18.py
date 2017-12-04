4dbafe6c6f0a5c8923e12e2e6c702db0689d2185
cdpo224UKY <31912114+cdpo224UKY@users.noreply.github.com>
2017-12-03 22:20:45 -0500

    activities 17 and 18

--- Activities/activity_18.py
+++ Activities/activity_18.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 import os
 import sys
-import numpy
 
 curr_path = os.path.realpath(__file__)
 CHE550_path = curr_path[: curr_path.find('\\', curr_path.find("CHE550"))]
@@ -9,7 +8,7 @@ sys.path.append(CHE550_path)
 
 import CHE550_tools
 
-identifiers = [
+identifiers = (
     ">1 dna:chromosome chromosome:GRCh38:1:1:248956422:1 REF",
     ">10 dna:chromosome chromosome:GRCh38:10:1:133797422:1 REF",
     ">11 dna:chromosome chromosome:GRCh38:11:1:135086622:1 REF",
@@ -32,47 +31,39 @@ identifiers = [
     ">7 dna:chromosome chromosome:GRCh38:7:1:159345973:1 REF",
     ">8 dna:chromosome chromosome:GRCh38:8:1:145138636:1 REF",
     ">9 dna:chromosome chromosome:GRCh38:9:1:138394717:1 REF",
+    ">MT dna:chromosome chromosome:GRCh38:MT:1:16569:1 REF",
     ">X dna:chromosome chromosome:GRCh38:X:1:156040895:1 REF",
     ">Y dna:chromosome chromosome:GRCh38:Y:2781480:56887902:1 REF"
-]
+)
 
 if __name__ == '__main__':
 
     chromosomes = dict()
+    chrom_id = str()
 
     with open('F:/Homo_sapiens.GRCh38.dna.primary_assembly.fa', 'r') as f:
-
-        chrom_id = str()
-        end_t = False  # used incase the line ends with a T and the next line starts with T
 
         for line in f:
             # found header for chromosome
             if any(ident in line for ident in identifiers):
                 if chrom_id:
-                    print(chromosomes[chrom_id])
+                    print(chromosomes[chrom_id][0] / sum(chromosomes[chrom_id]),
+                          chromosomes[chrom_id][1] / sum(chromosomes[chrom_id]),
+                          sum(chromosomes[chrom_id]))
                 print(line, end='')
-                chrom_id = line  # set sentinel value
-                chromosomes[chrom_id] = int()
-                end_t = False
-            # found a header other than chromosome
+                chrom_id = line
+                chromosomes[chrom_id] = [0, 0]
+            # found other header
             elif '>' in line:
-                print(line, end='')
                 if chrom_id:
-                    print(chromosomes[chrom_id])
+                    print(chromosomes[chrom_id][0]/sum(chromosomes[chrom_id]),
+                          chromosomes[chrom_id][1]/sum(chromosomes[chrom_id]),
+                          sum(chromosomes[chrom_id]))
                 chrom_id = str()
-                end_t = False
-            # sequence
             elif chrom_id:
-                chromosomes[chrom_id] += len(CHE550_tools.DNA.find_dimer_sites(line))
-                if line[0] == "T":
-                    chromosomes[chrom_id] += 1
-                if "T\n" in line:
-                    end_t = True
-                else:
-                    end_t = False
-
-    data = [chromosomes[chrom_id] for chrom_id in chromosomes]
-    print("# dimer sites: " + str(numpy.sum(data)))
-    print("AVG: " + str(numpy.mean(data)))
-    print("STD: " + str(numpy.std(data)))
-    print("Median: " + str(numpy.median(data)))
+                # A or T
+                chromosomes[chrom_id][0] += line.count('A')
+                chromosomes[chrom_id][0] += line.count('T')
+                # G or C
+                chromosomes[chrom_id][1] += line.count('G')
+                chromosomes[chrom_id][1] += line.count('C')
